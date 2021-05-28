e872009e1271124f36e338122373e13815dbfbaf
Mauro de Carvalho <mauro.dcg@gmail.com>
2021-05-28 00:54:09 -0300

    Dia 8: JavaScript: Fundamentos

---
+++ exercicios-js/fundamentos/35-logicos.js
@@ -0,0 +1,13 @@
+function compras(trabalho1, trabalho2) {
+    const comprarSorvete = trabalho1 || trabalho2
+    const comprarTv50 = trabalho1 && trabalho2
+    // const comprarTv32 = !!(trabalho1 ^ trabalho2) // bitwise xor
+    const comprarTv32 = trabalho1 != trabalho2
+    const manterSaudavel = !comprarSorvete // operador unário
+    return { comprarSorvete, comprarTv50, comprarTv32, manterSaudavel } // Criação de objetos implicito, ainda é criado chave valor
+}
+
+console.log(compras(true, true))
+console.log(compras(true, false))
+console.log(compras(false, true))
+console.log(compras(false, false))
