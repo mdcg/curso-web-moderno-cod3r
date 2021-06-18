874a4a90d4a6324a97728b945c219a0b1f717821
Mauro de Carvalho <mauro.dcg@gmail.com>
2021-06-17 22:40:43 -0300

    Dia 16 - JavaScript: Objetos

---
+++ exercicios-js/objetos/1-oo.js
@@ -0,0 +1,23 @@
+// CÓDIGO NÃO EXECUTÁVEL
+
+// Procedural
+processamento(valor, valor2, valor3)
+
+// OO
+obj = {
+    valor1,
+    valor2,
+    valor3,
+    processamento () {
+        // ...
+    }
+}
+
+obj.processamento() // foco passou a ser o objeto
+
+// Principios:
+
+// 1. Abstração
+// 2. Encapsulamento
+// 3. Herança (Prototype)
+// 4. Polimorfismo
